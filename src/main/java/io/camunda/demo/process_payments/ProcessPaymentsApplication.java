2293f14604d884c169de37eb1eb41b951b6d463d
Steven Hicks <steven.hicks@camunda.com>
2024-05-30 12:31:16 -0500

    Step 1: Create a new Spring Boot project

---
+++ src/main/java/io/camunda/demo/process_payments/ProcessPaymentsApplication.java
@@ -0,0 +1,13 @@
+package io.camunda.demo.process_payments;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProcessPaymentsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProcessPaymentsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/camunda/demo/process_payments/ProcessPaymentsApplicationTests.java
@@ -0,0 +1,13 @@
+package io.camunda.demo.process_payments;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProcessPaymentsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
