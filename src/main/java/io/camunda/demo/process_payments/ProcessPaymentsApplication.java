92b0d77f6cbab555cfb9e2d120b55730a2ba973a
Steven Hicks <steven.hicks@camunda.com>
2024-05-30 15:49:10 -0500

    Step 4: Start a process instance

--- src/main/java/io/camunda/demo/process_payments/ProcessPaymentsApplication.java
+++ src/main/java/io/camunda/demo/process_payments/ProcessPaymentsApplication.java
@@ -1,13 +1,38 @@
 package io.camunda.demo.process_payments;
 
+import java.util.Map;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import io.camunda.zeebe.client.ZeebeClient;
+
 @SpringBootApplication
-public class ProcessPaymentsApplication {
+public class ProcessPaymentsApplication implements CommandLineRunner {
+
+	private static final Logger LOG = LoggerFactory.getLogger(ProcessPaymentsApplication.class);
+
+	@Autowired
+	private ZeebeClient zeebeClient;
 
 	public static void main(String[] args) {
 		SpringApplication.run(ProcessPaymentsApplication.class, args);
+	}
+
+	@Override
+	public void run(final String... args) {
+		var processDefinitionKey = "process-payments"; // or whatever the key is
+		var event = zeebeClient.newCreateInstanceCommand()
+				.bpmnProcessId(processDefinitionKey)
+				.latestVersion()
+				.variables(Map.of("total", 100))
+				.send()
+				.join();
+		LOG.info(String.format("started a process: %d", event.getProcessInstanceKey()));
 	}
 
 }
